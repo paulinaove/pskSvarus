dff0f382b8a2d420bf5e522f40d407b3cb8e1c0e
Čiukšys Donatas <donatas.ciuksys@mif.vu.lt>
2016-10-03 17:53:44 +0300

    Cache propagation

---
+++ src/main/java/lt/vu/asynchronous/CompB.java
@@ -0,0 +1,24 @@
+package lt.vu.asynchronous;
+
+import javax.ejb.AsyncResult;
+import javax.ejb.Asynchronous;
+import javax.ejb.Stateful;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.concurrent.Future;
+
+@SessionScoped
+@Stateful
+public class CompB implements Serializable {
+
+    @Asynchronous
+    public Future<String> sayHello() {
+        System.out.println("CompB starts working on a big task...");
+        try {
+            Thread.sleep(10000); // sleep for 10 seconds
+        } catch (InterruptedException e) {
+        }
+        return new AsyncResult<>("BIG result from component B :)");
+    }
+
+}
